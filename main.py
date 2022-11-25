dd249fb38cd91d359266187805b1da567213879f
ngvinhy <ngvinhy@users.noreply.github.com>
2022-11-25 08:50:31 +0700

    ok

---
+++ main.py
@@ -0,0 +1,14 @@
+n = int(input('Nhập vào số hạng N: '))
+f0 = 0
+f1 = 1
+fn = 1
+if n <= 0:
+    print('Số hạng không xác định')
+elif n == 1:
+    print(f'Số hạng thứ {n} trong dãy Fibonacci là: {n}')
+else:
+    for i in range(2, n):
+        f0 = f1
+        f1 = fn
+        fn = f0 + f1
+    print(f'Số hạng thứ {n} trong dãy Fibonacci là: {fn}')
