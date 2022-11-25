c4f4507f3b0dd7d83702350132696f3c3d83eb3c
ngvinhy <ngvinhy@users.noreply.github.com>
2022-11-25 22:34:00 +0700

    now

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from atm import atm
 title = 'CHƯƠNG TRÌNH RÚT TIỀN TẠI CÂY ATM'
 print(title.center(60))
-khachhang = ['Nguyễn Thị Thu Hiền', 'Nguyễn Vĩnh Ý', 'Đinh Hồng Cơ', 'Bùi Đăng Nguyên', 'Đỗ Thành Danh']
-tien = [30000000, 40000000, 50000000, 60000000, 70000000]
+khachhang1 = ['Hiền', 1, 30000000, 10000000]
+khachhang2 = ['Ý', 2, 40000000, 10000000]
+khachhang3 = ['Cơ', 3, 50000000, 10000000]
+khachhang4 = ['Nguyên', 4, 60000000, 10000000]
+khachhang5 = ['Danh', 5, 70000000, 10000000]
 name = str(input('Vui lòng nhập tên chủ tài khoản: '))
-if name == khachhang[0]:
-    atm(khachhang[0], 1, tien[0], 10000000)
-if name == khachhang[1]:
-    atm(khachhang[1], 2, tien[1], 10000000)
-if name == khachhang[2]:
-    atm(khachhang[2], 3, tien[2], 10000000)
-if name == khachhang[3]:
-    atm(khachhang[3], 4, tien[3], 10000000)
-if name == khachhang[4]:
-    atm(khachhang[4], 5, tien[4], 10000000)
+if name == khachhang1[0]:
+    atm(khachhang1[0], khachhang1[1], khachhang1[2], khachhang1[3])
+if name == khachhang2[0]:
+    atm(khachhang2[0], khachhang2[1], khachhang2[2], khachhang2[3])
+if name == khachhang3[0]:
+    atm(khachhang3[0], khachhang3[1], khachhang3[2], khachhang3[3])
+if name == khachhang4[0]:
+    atm(khachhang4[0], khachhang4[1], khachhang4[2], khachhang4[3])
+if name == khachhang5[0]:
+    atm(khachhang5[0], khachhang5[1], khachhang5[2], khachhang5[3])
