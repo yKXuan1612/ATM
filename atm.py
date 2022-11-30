7c4411f59bc96c359076d0752ab68c76881a4f8d
ngvinhy <ngvinhy@users.noreply.github.com>
2022-11-30 22:45:56 +0700

    ngvinhy

--- atm.py
+++ atm.py
@@ -1,43 +1,47 @@
-def atm(tk, password, tien, gioihanrut, changesodu, changehanmuc, stt):
+def atm(tk, password, tien, gioihanrut, changesodu, changehanmuc, stt, sodu_atm):
+    print(sodu_atm)
     mapin = int(input('Hãy nhập mã PIN, nếu muốn ngừng sử dụng dịch vụ hãy nhập số 0: '))
     dem = 0
     k = 3
     while True:
         if mapin == 0:
             break
-        if mapin != password:
+        elif mapin != password:
             dem += 1
             if dem == 3:
                 print('Bạn đã nhập sai mã PIN quá số lần quy định')
                 break
-        if mapin == password:
+        elif mapin == password:
             print(f'Chủ thẻ: {tk} \nSố dư: {tien} VNĐ \nHạn mức giao dịch: {gioihanrut} VNĐ')
             ruttien = int(input('Hãy nhập số tiền cần rút: '))
             if ruttien == 0:
                 break
-            if ruttien <= tien:
+            elif ruttien > sodu_atm[0]:
+                print('ATM không đủ tiền thực hiện giao dịch, xin vui lòng thử lại')
+            elif ruttien <= tien:
                 if gioihanrut < ruttien:
                     print('Số tiền rút vượt quá hạn mức giao dịch, xin vui lòng thử lại')
                 else:
                     if ruttien % 50000 == 0:
                         tien = tien - ruttien
                         changesodu[stt] = tien
+                        sodu_atm[0] -= ruttien
                         print(f'Bạn đã rút thành công {ruttien} VNĐ \nSố dư: {tien} VNĐ')
                         gioihanrut = gioihanrut - ruttien
                         changehanmuc[stt] = gioihanrut
                         if gioihanrut == 0:
                             break
                         q = str(input('Bạn có muốn tiếp tục giao dịch không: '))
-                        if q == 'Không':
+                        if q == 'Không' or q == 'Ko' or q == 'ko' or q == 'không' or q == 'k' or q == 'K':
                             bienlai = str(input('Bạn có muốn nhận hóa đơn không: '))
-                            if bienlai == 'Có':
+                            if bienlai == 'Có' or bienlai == 'có':
                                 print(f'Chủ thẻ: {tk} \nSố tiền đã giao dịch: {ruttien} \nSố dư: {tien} VNĐ')
                                 break
-                            if bienlai == 'Không':
+                            if bienlai == 'Không' or bienlai == 'Ko' or bienlai == 'ko' or bienlai == 'không' or bienlai == 'k' or bienlai == 'K':
                                 break
                     else:
                         q = str(input('Số tiền giao dịch phải là bội số của 50000, bạn có muốn tiếp tục giao dịch khác không: '))
-                        if q == 'Không':
+                        if q == 'Không' or q == 'Ko' or q == 'ko' or q == 'không' or q == 'k' or q == 'K':
                             break
             else:
                 print('Bạn không có đủ tiền trong tài khoản, xin vui lòng thử lại')
--- main.py
+++ main.py
@@ -2,23 +2,17 @@ from atm import atm
 title = 'CHƯƠNG TRÌNH GIAO DỊCH TẠI CÂY ATM'
 print(title.center(60))
 tenkh = ['Nguyễn Vĩnh Ý', 'Nguyễn Thị Thu Hiền', 'Đinh Hồng Cơ', 'Bùi Đăng Nguyên', 'Đỗ Thành Danh']
-tentk = ['ngvinhy', 'thuhien123', 'hongcodinh', 'nguyenbui', 'danh555']
+tentk = ['y', 'hien', 'co', 'nguyen', 'danh']
 mk = [1, 2, 3, 4, 5]
 sodu = [30000000, 40000000, 50000000, 60000000, 70000000]
 hanmuc = [5000000, 10000000, 15000000, 20000000, 30000000]
-name = str(input('Hãy nhập tên tài khoản, nếu muốn ngừng sử dụng dịch vụ hãy nhập số 0: '))
-k = 3
+nganhang = [10000000]
 while True:
+    name = str(input('Hãy nhập tên tài khoản, nếu muốn ngừng sử dụng dịch vụ hãy nhập số 0: '))
     if name in tentk:
         i = tentk.index(name)
-        atm(tenkh[i], mk[i], sodu[i], hanmuc[i], sodu, hanmuc, i)
+        atm(tenkh[i], mk[i], sodu[i], hanmuc[i], sodu, hanmuc, i, nganhang)
     if name == '0':
         break
-    if name not in tentk:
-        k -= 1
-        if k == 0:
-            print('Bạn đã nhập sai tên tài khoản quá số lần quy định')
-            break
-        name = str(input(f'Xin vui lòng nhập lại tên tài khoản, bạn còn {k} lần: '))
-    else:
-        name = str(input('Hãy nhập tên tài khoản, nếu muốn ngừng sử dụng dịch vụ hãy nhập số 0: '))
+    if nganhang[0] <= 0:
+        break
